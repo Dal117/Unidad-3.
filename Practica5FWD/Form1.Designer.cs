81d682a8f91db30dcaafd86f7e8fdb286fe3ebf9
Dal117 <gonzalezpena.david.2024@gmail.com>
2025-06-02 18:42:37 -0500

    Add files via upload

---
+++ Practica5FWD/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Practica5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(129, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(560, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Genera los números del 1 al 20 con While";
+            label1.Click += label1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.InactiveCaption;
+            listBox1.Font = new Font("Segoe UI Symbol", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 28;
+            listBox1.Location = new Point(251, 103);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(455, 284);
+            listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Showcard Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(45, 200);
+            button1.Name = "button1";
+            button1.Size = new Size(162, 87);
+            button1.TabIndex = 2;
+            button1.Text = "Imprime en pantalla";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 224, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            ForeColor = SystemColors.MenuText;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
---
+++ Practica8FWD/Practica8/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Practica8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(166, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(464, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Este programa escribe del 1 al 5";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.InactiveBorder;
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(199, 103);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(398, 324);
+            listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightSalmon;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(621, 207);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 90);
+            button1.TabIndex = 2;
+            button1.Text = "Listar números";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Linen;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
